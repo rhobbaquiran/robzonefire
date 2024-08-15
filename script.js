23427b5c6fb08399c3fe76e253d9732721e0b7d2
rhobbaquiran <106058139+rhobbaquiran@users.noreply.github.com>
2024-08-15 23:15:02 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+
+    menuToggle.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
+});
